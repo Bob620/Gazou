b32f8302a913a2db1930ade5a33268224e14aaca
Bob620 <bruder.kraft225@gmail.com>
2017-10-25 00:00:36 -0500

    Modularization started

--- index.js
+++ index.js
@@ -186,7 +186,7 @@ function processDir(directory, options) {
           params.ExclusiveStartKey = data.LastEvaluatedKey;
         }
 
-        Promise.all(imagePromises).then(() => {
+        Promise.all(imagePromises).then(async () => {
           console.log(`Downloaded remote store containing ${remoteStore.size} images`);
           console.log(`Remote images retrived in ${process.uptime() - startDownloadTime} secs\n`);
           console.log('Comparing local and remote stores\n');
@@ -222,141 +222,217 @@ function processDir(directory, options) {
 
 //          console.log(`Unique images found: ${localStore.size}\n`);
           
-          fs.writeFile(`${directory}/localStore.json`, JSON.stringify([...localStore]), (err) => {
-            if (err) {
-              console.log('Unable to update the local store\n');
-            } else {
-              console.log('Local store updated\n');
-              console.log(`Full scan, comparison, and update completed in ${process.uptime() - startTime} sec\n`);
+          // Abstract this section
 
-              let maxNewUploads = options.max < newToSync.length ? options.max : newToSync.length;
+          // Writes a file for the local store
+          // Handle a better way
+          fs.writeFileSync(`${directory}/localStore.json`, JSON.stringify([...localStore]));
+          
+          console.log('Local store updated\n');
+          console.log(`Full scan, comparison, and update completed in ${process.uptime() - startTime} sec\n`);
 
-              let totalUploaded = 0;
-              const uploadBar = new progressBar();
+          // Find the max number of new items to upload
+          let maxNewUploads = options.max < newToSync.length ? options.max : newToSync.length;
+
+          // UPDATE OLD FILES
+          // Updates all of old files or none of them
+          if (options.update && diffToSync.length > 0) {
+            console.log('Updating old images...');
+            
+            // Init progress bar
+            let totalUploaded = 0;
+            const uploadBar = new progressBar({total: diffToSync.length});
+            uploadBar.update(0);
+
+            // Sync all the images for differences
+            for (let i = 0; i < diffToSync.length; i++) {
+              pushUpdate(diffToSync[i]).then(({remoteImage}) => {
+                uploadBar.update(++totalUploaded);
+              });
+            }
+          }
+
+          // Uploads max number of new items
+          if (options.push && newToSync.length > 0) {
+            console.log('Pushing new images...');
+            
+            // Init progress bar
+            let totalUploaded = 0;
+            const uploadBar = new progressBar({total: maxNewUploads});
+            uploadBar.update(0);
+
+            // Iterate over all the objects needingto be uploaded
+            for (let i = 0; i < maxNewUploads; i++) {
+              // Find the image to upload
+              uploadImage(newToSync[i]).then(({s3Details, remoteImage}) => {
+                uploadBar.update(++totalUploaded);
+              });
+            }
+          }
+/*
+          // UPDATE OLD FILES
+          // Updates all of them or none of them
+          if (options.update && diffToSync.length > 0) {
+            console.log('Updating old images...');
+            // Init progress bar
+            let totalUploaded = 0;
+            const uploadBar = new progressBar({total: diffToSync.length});
+            uploadBar.update(0);
+
+            // Handle async processes better
+            new Promise((resolve, reject) => {
+              // Sync all the images for differences
+              for (let i = 0; i < diffToSync.length; i++) {
+                const image = diffToSync[i];
+
+                // Updates the dynamodb item's tags
+                dynamodb.updateItem({
+                  ExpressionAttributeNames: {
+                    '#tags': 'tags'
+                  },
+                  ExpressionAttributeValues: {
+                    ':tags': {SS: image.tags}
+                  },
+                  Key: {
+                    'uid': {S: image.hash}
+                  },
+                  UpdateExpression: "SET #tags = :tags",
+                  TableName: 'picturebase'
+                }).then((data) => {
+                  // Updates the progress bar
+                  uploadBar.update(++totalUploaded);
+                  if (totalUploaded === diffToSync.length) {
+                    resolve();
+                  }
+                }).catch((err) => {
+                  // Error occured during update of an item
+                  // Create more in-depth logging and recovery
+                  throw err;
+                });
+              }
+            }).then(() => {
+              // Successful update of the 
+              console.log('\nOld images updated');
               
-              if (options.update && diffToSync.length > 0) {
-                console.log('Updating old images...');
-                uploadBar.update(diffToSync.length, totalUploaded);
+              // Uploads max number of new items
+              if (options.push && newToSync.length > 0) {
+                console.log('\nPushing new images...');
 
+                // Init progress bar
+                let totalUploaded = 0;
+                const uploadBar = new progressBar({total: maxNewUploads});
+                uploadBar.update(0);
+
+                // Handle async processes better
                 new Promise((resolve, reject) => {
-                  for (let i = 0; i < diffToSync.length; i++) {
-                    const image = diffToSync[i];
+                  // Iterate over all the objects needingto be uploaded
+                  for (let i = 0; i < maxNewUploads; i++) {
+                    // Find the image to upload
+                    const image = newToSync[i];
+                    const imageUrl = image.hash + image.localCopies[0].substr(image.localCopies[0].lastIndexOf('.')).toLowerCase();
 
+                    // Upload the image into the S3 storage
+                    s3Upload.push(image.localCopies[0], imageUrl).then((details) => {
+                      // Once the image is uploaded, upload the dynamodb link to it
+                      dynamodb.updateItem({
+                        ExpressionAttributeNames: {
+                          '#tags': 'tags',
+                          '#url': 'url'
+                        },
+                        ExpressionAttributeValues: {
+                          ':tags': {SS: image.tags},
+                          ':url': {S: imageUrl}
+                        },
+                        Key: {
+                          'uid': {S: image.hash}
+                        },
+                        UpdateExpression: "SET #tags = :tags, #url = :url",
+                        TableName: 'picturebase'
+                      }).then((data) => {
+                        // Update the progress bar
+                        uploadBar.update(++totalUploaded);
+                        if (totalUploaded === maxNewUploads) {
+                          resolve();
+                        }
+                      }).catch((err) => {
+                        // Need better error handling and recovery
+                        throw err;
+                      });
+                    }).catch((err) => {
+                      // Need better error handling and recovery
+                      throw err;
+                    })
+                  }
+                }).then(() => {
+                  // All images uploaded successfully
+                  console.log('\nImages pushed');
+                }).catch((err) => {
+                  // Need better error handling and recovery
+                  throw err;
+                });
+              }
+            }).catch(() => {
+              // Error occurs in uploading updated tags
+            });
+          } else {
+            // Uploads max number of new items
+            if (options.push && newToSync.length > 0) {
+              console.log('\nPushing new images...');
+              // Init progress bar
+              let totalUploaded = 0;
+              const uploadBar = new progressBar({total: maxNewUploads});
+              uploadBar.update(0);
+
+              // Handle async processes better
+              new Promise((resolve, reject) => {
+                // Iterate through all images to upload
+                for (let i = 0; i < maxNewUploads; i++) {
+                  // Locate the image to upload
+                  const image = newToSync[i];
+                  const imageUrl = image.hash + image.localCopies[0].substr(image.localCopies[0].lastIndexOf('.')).toLowerCase();
+
+                  // Upload image to S3 storage
+                  s3Upload.push(image.localCopies[0], imageUrl).then((details) => {
+                    // Once the image is uploaded, upload the dynamodb link to it
                     dynamodb.updateItem({
                       ExpressionAttributeNames: {
-                        '#tags': 'tags'
+                        '#tags': 'tags',
+                        '#url': 'url'
                       },
                       ExpressionAttributeValues: {
-                        ':tags': {SS: image.tags}
+                        ':tags': {SS: image.tags},
+                        ':url': {S: imageUrl}
                       },
                       Key: {
                         'uid': {S: image.hash}
                       },
-                      UpdateExpression: "SET #tags = :tags",
+                      UpdateExpression: "SET #tags = :tags, #url = :url",
                       TableName: 'picturebase'
                     }).then((data) => {
-                      uploadBar.update(diffToSync.length, ++totalUploaded);
-                      if (totalUploaded === diffToSync.length) {
+                      // Update progress bar
+                      uploadBar.update(++totalUploaded);
+                      if (totalUploaded === maxNewUploads) {
                         resolve();
                       }
                     }).catch((err) => {
+                      // Need better error handling and recovery
                       throw err;
                     });
-                  }
-                }).then(() => {
-                  console.log('\nOld images updated');
-                  
-                  if (options.push && newToSync.length > 0) {
-                    console.log('\nPushing new images...');
-                    totalUploaded = 0;
-                    uploadBar.update(maxNewUploads, totalUploaded);
-    
-                    new Promise((resolve, reject) => {
-                      for (let i = 0; i < maxNewUploads; i++) {
-                        const image = newToSync[i];
-                        const imageUrl = image.hash + image.localCopies[0].substr(image.localCopies[0].lastIndexOf('.')).toLower();
-
-                        s3Upload.push(image.localCopies[0], imageUrl).then((details) => {
-                          dynamodb.updateItem({
-                            ExpressionAttributeNames: {
-                              '#tags': 'tags',
-                              '#url': 'url'
-                            },
-                            ExpressionAttributeValues: {
-                              ':tags': {SS: image.tags},
-                              ':url': {S: imageUrl}
-                            },
-                            Key: {
-                              'uid': {S: image.hash}
-                            },
-                            UpdateExpression: "SET #tags = :tags, #url = :url",
-                            TableName: 'picturebase'
-                          }).then((data) => {
-                            uploadBar.update(maxNewUploads, ++totalUploaded);
-                            if (totalUploaded === maxNewUploads) {
-                              resolve();
-                            }
-                          }).catch((err) => {
-                            throw err;
-                          });
-                        }).catch((err) => {
-                          throw err;
-                        })
-                      }
-                    }).then(() => {
-                      console.log('\nImages pushed');
-                    }).catch((err) => {
-                      throw err;
-                    });
-                  }
-                }).catch(() => {
-                });
-              } else {
-                if (options.push && newToSync.length > 0) {
-                  console.log('\nPushing new images...');
-                  totalUploaded = 0;
-                  uploadBar.update(maxNewUploads, totalUploaded);
-  
-                  new Promise((resolve, reject) => {
-                    for (let i = 0; i < maxNewUploads; i++) {
-                      const image = newToSync[i];
-                      const imageUrl = image.hash + image.localCopies[0].substr(image.localCopies[0].lastIndexOf('.')).toLower();
-
-                      s3Upload.push(image.localCopies[0], imageUrl).then((details) => {
-                        dynamodb.updateItem({
-                          ExpressionAttributeNames: {
-                            '#tags': 'tags',
-                            '#url': 'url'
-                          },
-                          ExpressionAttributeValues: {
-                            ':tags': {SS: image.tags},
-                            ':url': {S: imageUrl}
-                          },
-                          Key: {
-                            'uid': {S: image.hash}
-                          },
-                          UpdateExpression: "SET #tags = :tags, #url = :url",
-                          TableName: 'picturebase'
-                        }).then((data) => {
-                          uploadBar.update(maxNewUploads, ++totalUploaded);
-                          if (totalUploaded === maxNewUploads) {
-                            resolve();
-                          }
-                        }).catch((err) => {
-                          throw err;
-                        });
-                      }).catch((err) => {
-                        throw err;
-                      })
-                    }
-                  }).then(() => {
-                    console.log('\nImages pushed');
-                  }).catch(() => {
-                  });
+                  }).catch((err) => {
+                    // Need better error handling and recovery
+                    throw err;
+                  })
                 }
-              }
+              }).then(() => {
+                // All images uploaded successfully
+                console.log('\nImages pushed');
+              }).catch((err) => {
+                // Need better error handling and recovery
+                console.log(err);
+              });
+            }
+          }
 /*
-              
               console.log('Syncing new images...');
               totalUploaded = 0;
               uploadBar.lastLength = 0;
@@ -370,9 +446,6 @@ function processDir(directory, options) {
                 totalUploaded++;
               }, 5);
 */
-            }
-          });
-
         }).catch((err) => {
           console.log(err);
         });  
@@ -411,6 +484,37 @@ function processDir(directory, options) {
   });
 }
 
+
+
+
+
+/*
+for (let i = 0; i < maxNewUploads; i++) {
+  console.log('\nUploading new images...');
+  totalUploaded = 0;
+  uploadBar.update(maxNewUploads, totalUploaded);
+
+  uploadImage(newToSync[i]).then(() => {
+    uploadBar.update(maxNewUploads, ++totalUploaded);
+    if (totalUploaded === maxNewUploads) {
+      resolve();
+    }
+  });
+
+  
+  console.log('Updating old images...');
+  uploadBar.update(diffToSync.length, totalUploaded);
+
+    for (let i = 0; i < diffToSync.length; i++) {
+      const image = diffToSync[i];
+
+        uploadBar.update(diffToSync.length, ++totalUploaded);
+        if (totalUploaded === diffToSync.length) {
+          resolve();
+        }
+}
+*/
+
 function compareTags(imageOne, imageTwo) {
   for (let i = 0; i < imageOne.tags.length; i++) {
     if (!imageTwo.tags.includes(imageOne.tags[i])) {
@@ -420,14 +524,67 @@ function compareTags(imageOne, imageTwo) {
   return false;
 }
 
-class progressBar{
-  constructor(barLength=20) {
+// Uploads a single image
+async function uploadImage({hash: imageHash, localCopies, tags}) {
+  // Get image name (imageHash + extention of original image)
+  // Currently uses the first local copy found
+  // Can we prefer .png over .jpg/.jpeg, vice versa?
+  // Technically they are the same image down to every bit because we checked
+  const localCopy = localCopies[0];
+  const imageUrl = imageHash + localCopy.substr(localCopy.lastIndexOf('.')).toLowerCase();
+
+  // Upload image to s3
+  const s3Details = await s3Upload.push(localCopy, imageUrl);  
+  // Upload image link to dynamodb
+  const remoteImage = await dynamodb.updateItem({
+    ExpressionAttributeNames: {
+      '#tags': 'tags',
+      '#url': 'url'
+    },
+    ExpressionAttributeValues: {
+      ':tags': {SS: tags},
+      ':url': {S: imageUrl}
+    },
+    Key: {
+      'uid': {S: imageHash}
+    },
+    UpdateExpression: "SET #tags = :tags, #url = :url",
+    TableName: 'picturebase'
+  });
+  // Return something
+  return {s3Details, remoteImage};
+}
+
+// Updates a single image's dynamodb entry 
+async function pushUpdate({hash: imageHash, tags}) {
+  // Only a hash is needed to update the image link
+  // Once uploaded you can't modify the image it points at (the url)
+  const remoteImage = await dynamodb.updateItem({
+    ExpressionAttributeNames: {
+      '#tags': 'tags'
+    },
+    ExpressionAttributeValues: {
+      ':tags': {SS: tags}
+    },
+    Key: {
+      'uid': {S: imageHash}
+    },
+    UpdateExpression: "SET #tags = :tags",
+    TableName: 'picturebase'
+  });
+  // Return something
+  return {remoteImage};
+}
+
+class progressBar {
+  constructor({barLength=20, total=100}) {
+    this.total = total;
     this.barLength = barLength;
     this.lastLength = 0;
   }
 
-  update(total, finished) {
-    const percentComplete = finished/total;
+  update(finished) {
+    const percentComplete = finished/this.total;
     const bars = Math.floor(percentComplete*this.barLength);
     let bar = '=>';
 
@@ -442,7 +599,7 @@ class progressBar{
       }
     }
 
-    let uploadProcess = `[${bar}] [${(percentComplete*100).toFixed(2)}%] ${finished}/${total}`;
+    let uploadProcess = `[${bar}] [${(percentComplete*100).toFixed(2)}%] ${finished}/${this.total}`;
     
     for (let i = 0; i < this.lastLength; i++) {
       uploadProcess = '\b' + uploadProcess;
